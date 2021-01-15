4ffd22b1fd388f072a56679a52d6f0ef225dfa94
Neil Earlam <neil.earlam2019@outlook.com>
2021-01-15 19:18:10 +0000

    Base Api Project

---
+++ PhotoProject/PhotoProject.WebApi/Controllers/PhotoController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PhotoProject.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PhotoController : ControllerBase
+    {
+        public PhotoController()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok("HELLO IS IT ME YOUR LOOKING FOR");
+        }
+    }
+}
